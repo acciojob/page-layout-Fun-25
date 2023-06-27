682379df18cd93f839f7778d0db72f394025d5c8
Fun-25 <muskandodmani760@gmail.com>
2023-06-27 18:39:13 +0530

    submitting code-Tue Jun 27 2023 18:39:11 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,23 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <PageLayout
+        header="Welcome to my website"
+        children="This is the content of my website"
+        footer={`\u00A9 2023 My Website. All rights reserved.`}
+      />
     </div>
-  )
+  );
 }
 
 export default App
+
+const PageLayout=({header,children,footer})=>{
+  return(
+    <div>
+      <h1>{header}</h1>
+      <p>{children}</p>
+      <p>{footer}</p>
+    </div>
+  )
+}
